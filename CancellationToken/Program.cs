c075a29c810b919bbace830fb5881f208b6d2f88
selimmemduhoglu <mselimmemduhoglu@gmail.com>
2024-04-11 21:39:21 +0300

    Add project files.

---
+++ CancellationToken/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using CancellationTokenTimeOut;
+
+string connStr = "Server=mylocalhost;Database=test_db;User Id=sa;Password=sa;Connection Timeout=8";
+var manager = new SqlServerManager();
+
+var now = DateTime.Now;
+
+var tokenSource = new CancellationTokenSource();
+//var tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(3));
+//tokenSource.CancelAfter(TimeSpan.FromSeconds(3));
+var token = tokenSource.Token;
+
+//_ = Task.Run(async () =>
+//{
+//    await Task.Delay(3_000);
+//    tokenSource.Cancel();
+//});
+
+try
+{
+    await manager.Connect(connStr, token);
+}
+catch (TaskCanceledException tcex)
+{
+    Console.WriteLine("Task CANCELLED");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Total MS" + DateTime.Now.Subtract(now).TotalMilliseconds);
+}
---
+++ CancellationToken/SqlServerManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.SqlClient;
+
+namespace CancellationTokenTimeOut;
+
+public class SqlServerManager
+{
+    public async Task Connect(string connectionString, CancellationToken cancellationToken)
+    {
+        SqlConnection connection = new(connectionString);
+        await connection.OpenAsync(cancellationToken);
+    }
+}
